7c44b045b4312fec9ecacc0c0199e0e80e7881d0
Stoeffel <schtoeffel@gmail.com>
2016-04-20 20:48:59 +0200

    adds tests for namespacedConstants

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import compose from 'compose-function';
-import constants, {namespace} from './';
+import constants, {namespace, namespacedConstants} from './';
 
 test('create-constants', t => {
   t.deepEqual(constants([]), {});
@@ -14,4 +14,10 @@ test('namespace', t => {
   t.deepEqual(myNameConstants([]), {});
   t.deepEqual(myNameConstants(['UNICORN', 'RAINBOWS']), { UNICORN: 'my/name/UNICORN', RAINBOWS: 'my/name/RAINBOWS' });
   t.deepEqual(myNameConstants(['add todo', 'removeTodo']), { ADD_TODO: 'my/name/ADD_TODO', REMOVE_TODO: 'my/name/REMOVE_TODO' });
+});
+
+test('namespacedConstants', t => {
+  t.deepEqual(namespacedConstants('my/name/', []), {});
+  t.deepEqual(namespacedConstants('my/name/', ['UNICORN', 'RAINBOWS']), { UNICORN: 'my/name/UNICORN', RAINBOWS: 'my/name/RAINBOWS' });
+  t.deepEqual(namespacedConstants('my/name/', ['add todo', 'removeTodo']), { ADD_TODO: 'my/name/ADD_TODO', REMOVE_TODO: 'my/name/REMOVE_TODO' });
 });
